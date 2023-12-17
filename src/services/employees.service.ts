1bc444eaa8d2e5cf4047b0232ae2e5c6ed85b35b
raishudesu <bacaltosbaryshnikov@gmail.com>
2023-12-17 19:30:22 +0800

    added crud services

--- src/services/employees.service.ts
+++ src/services/employees.service.ts
@@ -1,4 +1,16 @@
 import prisma from "../utils/db.server";
+import type { employees } from "@prisma/client";
+
+export const addEmployee = async (data: employees) => {
+  try {
+    const res = await prisma.employees.create({
+      data,
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const getEmployees = async () => {
   try {
@@ -9,6 +21,48 @@ export const getEmployees = async () => {
     if (!employees) throw "No employees available";
 
     return employees;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getEmployee = async (idemployees: number) => {
+  try {
+    const employee = await prisma.employees.findFirst({
+      where: {
+        idemployees,
+      },
+    });
+    return employee;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteEmployee = async (idemployees: number) => {
+  try {
+    const res = await prisma.employees.delete({
+      where: {
+        idemployees,
+      },
+    });
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateEmployee = async (idemployees: number, data: employees) => {
+  try {
+    const res = await prisma.employees.update({
+      where: {
+        idemployees,
+      },
+      data,
+    });
+
+    return res;
   } catch (error) {
     throw error;
   }
